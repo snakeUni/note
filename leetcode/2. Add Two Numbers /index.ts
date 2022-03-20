4cbd82ff338ca75dc7d71c305577bc4de8192bef
Uni <1025687605@qq.com>
2022-03-20 16:45:03 +0800

    add

---
+++ leetcode/2. Add Two Numbers /index.ts
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  let head = null,
+    tail: any = null
+  let carry = 0
+  while (l1 || l2) {
+    const n1 = l1 ? l1.val : 0
+    const n2 = l2 ? l2.val : 0
+    const sum = n1 + n2 + carry
+
+    if (!head) {
+      head = tail = new ListNode(sum % 10)
+    } else {
+      tail.next = new ListNode(sum % 10)
+      tail = tail.next
+    }
+    carry = Math.floor(sum / 10)
+
+    if (l1) {
+      l1 = l1.next
+    }
+
+    if (l2) {
+      l2 = l2.next
+    }
+  }
+
+  if (carry > 0) {
+    tail.next = new ListNode(carry)
+  }
+
+  return head
+}
