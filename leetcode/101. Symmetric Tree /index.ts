860a533fe6f9548daba084f5aaefed953318528e
Uni <1025687605@qq.com>
2022-05-22 14:54:23 +0800

    feat: add 迭代

--- leetcode/101. Symmetric Tree /index.ts
+++ leetcode/101. Symmetric Tree /index.ts
@@ -11,12 +11,38 @@
  *     }
  * }
  */
-
+// https://leetcode.cn/problems/symmetric-tree/solution/dui-cheng-er-cha-shu-by-leetcode-solution/
 function isSymmetric(root: TreeNode | null): boolean {
   const check = (p: TreeNode | null, q: TreeNode | null): boolean => {
     if (!p && !q) return true
     if (!p || !q) return false
     return p.val === q.val && check(p.left, q.right) && check(p.right, q.left)
+  }
+
+  return check(root, root)
+}
+
+// 迭代方法
+// https://leetcode.cn/problems/symmetric-tree/solution/dui-cheng-er-cha-shu-by-leetcode-solution/
+function isSymmetric2(root: TreeNode | null): boolean {
+  const check = (u: TreeNode | null, v: TreeNode | null): boolean => {
+    const q: (TreeNode | null)[] = []
+    q.push(u), q.push(v)
+
+    while (q.length) {
+      u = q.shift()!
+      v = q.shift()!
+
+      if (!u && !v) continue
+      if (!u || !v || u.val !== v.val) return false
+
+      q.push(u.left)
+      q.push(v.right)
+
+      q.push(u.right)
+      q.push(v.left)
+    }
+    return true
   }
 
   return check(root, root)
