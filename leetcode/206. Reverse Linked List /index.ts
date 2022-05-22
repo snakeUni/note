bb553256d6caed5e2194ce15f7626d7333de3fb2
Uni <1025687605@qq.com>
2022-05-22 17:19:14 +0800

    feat: add 206

---
+++ leetcode/206. Reverse Linked List /index.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev = null
+  let curr = head
+  while (curr != null) {
+    let next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  }
+  return prev
+}
