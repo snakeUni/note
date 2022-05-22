246483183fe591bd9fa17dc2e137a9c4d2b08660
Uni <1025687605@qq.com>
2022-05-22 15:02:16 +0800

    feat: add 104

---
+++ leetcode/104. Maximum Depth of Binary Tree /index.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0
+  let leftHeight = maxDepth(root.left)
+  let rightHeight = maxDepth(root.right)
+  return Math.max(leftHeight, rightHeight) + 1
+}
